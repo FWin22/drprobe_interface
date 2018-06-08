0e3e447b4eea51d5b47e1f1eeb5bad70534b269f
Florian Winkler <flowinkler22@gmail.com>
2018-06-08 14:55:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="drprobe_interface",
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/FWin22/drprobe_interface/',
     author='Florian Winkler',
     author_email='flowinkler22@gmail.com',
